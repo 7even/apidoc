8325a1e59bd377c0b46774b099d484876bf055ed
Vsevolod Romashov <7@7vn.ru>
2013-03-21 02:38:35 +0400

    parsing rules for basic contexts

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,15 +2,56 @@ class Parser < Rly::Yacc
   lexer do
     token :VERB, /GET|POST|PUT|PATCH|DELETE/
     token :URL, /\/[A-Za-z0-9\-_\/.]+/
+    literals '<{}'
     
     ignore " \t\n"
+    
+    token :NUMBER, /\d+/ do |t|
+      t.value = t.value.to_i
+      t
+    end
+    
+    on_error do |t|
+      puts "Illegal character #{t.value}"
+      t.lexer.pos += 1
+      nil
+    end
   end
   
   rule 'requests : requests request | request' do |requests, *requests_array|
     requests.value = requests_array.map(&:value).flatten
   end
   
-  rule 'request : VERB URL' do |request, verb, url|
-    request.value = Request.new(verb: verb.value, url: url.value)
+  rule 'request : VERB URL contexts' do |request, verb, url, contexts|
+    request.value = Request.new do |r|
+      r.verb     = verb.value
+      r.url      = url.value
+      r.contexts = contexts.value
+    end
+  end
+  
+  rule 'contexts : contexts context | context' do |contexts, *contexts_array|
+    contexts.value = contexts_array.map { |c| Context.new(c.value) }
+  end
+  
+  rule 'context : context context_part | context_part' do |context, *context_parts|
+    context.value = context_parts.map(&:value).inject(:merge)
+  end
+  
+  rule 'context_part : request_body | response_code' do |context_part, part|
+    context_part.value = part.value
+  end
+  
+  rule 'request_body : json' do |request_body, json|
+    request_body.value = { request_body: json.value }
+  end
+  
+  rule 'response_code : "<" NUMBER' do |response_code, _, number|
+    response_code.value = { response_code: number.value }
+  end
+  
+  rule 'json : "{" "}"' do |json, *|
+    # TODO: working json pattern
+    json.value = ''
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,16 +2,28 @@ require 'spec_helper'
 
 describe Parser do
   it "parses basic requests" do
-    result = Parser.new.parse("GET /url\nPOST /other_url\nPUT /another_url")
+    source = <<-EOF
+      GET /url
+      < 200
+      POST /other_url
+      < 201
+      PUT /another_url
+      < 401
+    EOF
+    
+    result = Parser.new.parse(source)
     result.should have(3).requests
     
     result[0].verb.should == 'GET'
     result[0].url.should == '/url'
+    result[0].contexts.first.response_code.should == 200
     
     result[1].verb.should == 'POST'
     result[1].url.should == '/other_url'
+    result[1].contexts.first.response_code.should == 201
     
     result[2].verb.should == 'PUT'
     result[2].url.should == '/another_url'
+    result[2].contexts.first.response_code.should == 401
   end
 end
