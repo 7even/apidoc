396e1f30a2f1f8906a7385c3dce951d5114106eb
Vsevolod Romashov <7@7vn.ru>
2013-03-25 11:54:59 +0400

    preparing to deploy

---
+++ config/deploy.rb
@@ -0,0 +1,63 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :domain, '7erver'
+set :deploy_to, '/var/www/apidoc'
+set :user, 'web'
+
+set :repository, 'git://github.com/7even/apidoc.git'
+set :branch, 'master'
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['config/mongoid.yml']
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  invoke :'rbenv:load'
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  # queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+  
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+  
+  queue! %[touch "#{deploy_to}/shared/config/mongoid.yml"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    
+    to :launch do
+      queue 'mkdir -p tmp'
+      queue 'touch tmp/restart.txt'
+    end
+  end
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
