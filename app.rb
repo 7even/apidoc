42b8a71a0a859e22b30d99b431aee0068ab6844e
Vsevolod Romashov <7@7vn.ru>
2013-03-28 11:10:18 +0400

    edit page

--- app.rb
+++ app.rb
@@ -9,7 +9,12 @@ end
 get '/' do
   # TODO: distinguish the real current blueprint from the old
   @current_blueprint = Blueprint.last
-  slim :index
+  slim :show
+end
+
+get '/edit' do
+  @current_blueprint = Blueprint.last
+  slim :edit
 end
 
 post '/' do
